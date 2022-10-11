905944c751c0f148ca8b3ac4ee13460f7586111e
bachiito <riveraabraham332@gmail.com>
2022-10-11 16:57:50 -0400

    todo id fixed

--- js/model.js
+++ js/model.js
@@ -2,9 +2,11 @@ export default class Model {
     constructor() {
         this.view = null;
         this.todos = JSON.parse(localStorage.getItem("todos"));
-        this.currentId = !this.todos
-            ? 1
-            : this.todos[this.todos.length - 1].id + 1;
+        if (this.todos.length > 0) {
+            this.currentId = this.todos[this.todos.length - 1].id + 1;
+        } else {
+            this.currentId = 1;
+        }
     }
 
     setView(view) {
